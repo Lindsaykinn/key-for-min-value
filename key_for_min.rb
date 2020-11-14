1eb4bd54b9c8cd987ed9278ac105824cf696c7a0
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-14 13:39:34 -0500

    commit tree be994b79b3ff4305fb5ab9fbef75bfb2a5cd9561

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    lowest_item = nil
+    lowest_price = Float::INFINITY
+    name_hash.each do |item, price|
+    if price < lowest_price
+         lowest_price = price
+         lowest_item = item
+        end
+    end
+    lowest_item
 end
